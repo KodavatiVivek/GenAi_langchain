c75784b169a21984dd341e11c707f38d04b08bc3
AravindGarapati <aravind.garapati@gmail.com>
2026-04-21 12:41:30 +0100

    Added init_py to each folder and splitter code

---
+++ backend/app/ingestion/splitter.py
@@ -0,0 +1,16 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from .loader import load_documents
+from app.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+def split_documents(documents):
+    logger.info("Splitting documents into chunks...")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = text_splitter.split_documents(documents)
+    logger.info(f"Successfully split documents into {len(chunks)} chunks.")
+    return chunks
+
+if __name__ == "__main__":
+    documents = load_documents()
+    chunks = split_documents(documents)
